677affafaa6420da80920bb3d49032f0af8b52b0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-17 06:45:40 +0000

    Deploy Jayvirrathi/react-portfolio to github.com/Jayvirrathi/react-portfolio.git:gh-pages

---
+++ precache-manifest.a1e8d8de4d890e0c8a5d35c56b05e9ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56e112bfba7b51537b1ac7df19c9450",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "00ab6731b46d9e72a8f9",
+    "url": "/react-portfolio/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "4814629633024fe6e9fa",
+    "url": "/react-portfolio/static/css/main.59219617.chunk.css"
+  },
+  {
+    "revision": "00ab6731b46d9e72a8f9",
+    "url": "/react-portfolio/static/js/2.ab1c66d7.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-portfolio/static/js/2.ab1c66d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4814629633024fe6e9fa",
+    "url": "/react-portfolio/static/js/main.0ac0d958.chunk.js"
+  },
+  {
+    "revision": "9f289a7f9c113993272b",
+    "url": "/react-portfolio/static/js/runtime-main.f68edf73.js"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/react-portfolio/static/media/intro-logo.54e9ec53.png"
+  }
+]);
