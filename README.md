28074a91d14ed707402b9336a9d5d8e0b2b0b7a1
Jayvir Rathi <jayvirrathi@gmail.com>
2020-11-20 13:18:56 +0530

    added github workflow & removed travis

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
-# PORTFOLIO-APP 
-
+# PORTFOLIO-APP
 
 ## :rocket:Deployed Link : https://jayvirrathi.github.io/react-portfolio/
 
 ## ✨ Features
 
-- 📦 Build in React
+-   📦 Build in React
 
 ## Installation
 
 ```sh
-yarn add 
+yarn add
 # or
-npm install 
+npm install
 ```
 
-- **[ReactJS](https://reactjs.org/)** — contains the ["Getting Started" guide](https://reactjs.org/docs/getting-started.html) and all you need to know.
-- **[Travis-ci](https://travis-ci.org/)** — contains the ["Getting Started" guide](https://docs.travis-ci.com/) and all you need to know.
-- **[Github-pages](https://pages.github.com/)** — contains the ["Getting Started" guide](https://help.github.com/en/github/working-with-github-pages/getting-started-with-github-pages) and all you need to know.
-
+-   **[ReactJS](https://reactjs.org/)** — contains the ["Getting Started" guide](https://reactjs.org/docs/getting-started.html) and all you need to know.
+-   **[Github-Actions](https://github.com/marketplace?type=actions)** — contains the ["Getting Started" guide](https://docs.github.com/en/free-pro-team@latest/actions/quickstart) and all you need to know.
+-   **[Github-pages](https://pages.github.com/)** — contains the ["Getting Started" guide](https://help.github.com/en/github/working-with-github-pages/getting-started-with-github-pages) and all you need to know.
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,56 +1,72 @@
 const portfolioData = {
-  imageUrl: "http://localhost:3000/",
-  logo: "images/app-logo.svg",
-  logoWhite: "images/app-logo-white.svg",
-  name: "Jayvir Rathi",
-  role: "Full Stack Developer",
-  intro: "Let's keep building amazing stuff.",
-  projects: [
-    {
-      id: 0,
-      title: "WorkerHomeServices using React Native",
-      imageUrl: "images/portfolio/logo512.png",
-      link: "https://github.com/Jayvirrathi/WorkerHomeServices",
-      codeLink: "https://github.com/Jayvirrathi/WorkerHomeServices",
-      description:
-        "This projects fetches data from mongo atlas. It basically show worker their task. View and search info to next job .",
-      technology: ["HTML/CSS", "React Native", "MongoDB"],
-    },
-  ],
-  skills: [
-    {
-      name: "HTML/CSS",
-      percentage: 70,
-    },
-    {
-      name: "SASS/ SCSS",
-      percentage: 70,
-    },
-    {
-      name: "Javascript",
-      percentage: 80,
-    },
-    {
-      name: "REACT",
-      percentage: 70,
-    },
-    {
-      name: "NODEJS",
-      percentage: 70,
-    },
-    {
-      name: "Python",
-      percentage: 60,
-    },
-  ],
-  workExp: [
-    {
-      title: "Software Engineer Intern",
-      date: "Jan 2020- June 2020",
-      description: "Full Stack Developer",
-      company: "Innovify India.",
-    },
-  ],
+    imageUrl: 'http://localhost:3000/',
+    logo: 'images/app-logo.svg',
+    logoWhite: 'images/app-logo-white.svg',
+    name: 'Jayvir Rathi',
+    role: 'Full Stack Developer',
+    intro: "Let's keep building amazing stuff.",
+    projects: [
+        {
+            id: 0,
+            title: 'WorkerHomeServices using React Native',
+            imageUrl: 'images/portfolio/logo512.png',
+            link: 'https://github.com/Jayvirrathi/WorkerHomeServices',
+            codeLink: 'https://github.com/Jayvirrathi/WorkerHomeServices',
+            description:
+                'This projects fetches data from mongo atlas. It basically show worker their task. View and search info to next job .',
+            technology: ['HTML/CSS', 'React Native', 'MongoDB'],
+        },
+        {
+            id: 0,
+            title: 'Nodejs Nodejs Kubernetes Example',
+            imageUrl: 'images/portfolio/logo512.png',
+            link: 'https://github.com/Jayvirrathi/nodejs-docker-kubernetes',
+            codeLink: 'https://github.com/Jayvirrathi/nodejs-docker-kubernetes',
+            description:
+                'This projects example how to use docker & kubernetes with nodejs',
+            technology: ['Nodejs', 'Nodejs', 'Nodejs'],
+        },
+    ],
+    skills: [
+        {
+            name: 'HTML/CSS',
+            percentage: 70,
+        },
+        {
+            name: 'SASS/ SCSS',
+            percentage: 70,
+        },
+        {
+            name: 'Javascript',
+            percentage: 80,
+        },
+        {
+            name: 'REACTJS',
+            percentage: 70,
+        },
+        {
+            name: 'NODEJS',
+            percentage: 70,
+        },
+        {
+            name: 'Python',
+            percentage: 60,
+        },
+    ],
+    workExp: [
+        {
+            title: 'Software Engineer Intern',
+            date: 'Jan 2020- June 2020',
+            description: 'Full Stack Developer',
+            company: 'Innovify India.',
+        },
+        {
+            title: 'Software Engineer',
+            date: 'July 2020- Oct 2020',
+            description: 'Full Stack Developer',
+            company: 'Innovify India.',
+        },
+    ],
 };
 
 export default portfolioData;
